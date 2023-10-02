99149d84263b09788df2e36edf18959bb153396d
j.papontee <113662237+aatuodcrd@users.noreply.github.com>
2023-10-02 16:02:28 +0700

    Add files via upload

---
+++ Week 01/Week 01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Week 011/Week 011/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void solve(int n, char from, char to, char aux)
+    {
+        if (n == 0)
+            return;
+        solve(n - 1, from, aux, to);
+        Console.WriteLine("หยิบแผ่น " + n + " จาก " + from + " ไป " + to);
+        solve(n - 1, aux, to, from);
+    }
+
+    static int S(int n)
+    {
+        if (n == 0)
+            return 0;
+        return n + S(n - 1);
+    }
+
+    static int A(int n)
+    {
+        if (n == 1)
+            return 1;
+        return n * A(n - 1);
+    }
+
+    static void Main()
+    {
+        //solve(125, 'A', 'C', 'B'); //ตัวแรก
+        //Console.WriteLine(S(100)); //ตัวสอง
+        //Console.WriteLine(A(4));
+    }
+}
